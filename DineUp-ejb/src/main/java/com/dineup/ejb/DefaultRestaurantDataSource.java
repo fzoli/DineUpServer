21e540a37c65301c5c1c2940493eb33cb5d4bdc4
Zoltan Farkas <zoltan.farkas@cellum.com>
2015-11-25 00:10:31 +0100

    configure orm, implement data source, add servlet url to index page

--- DineUp-ejb/src/main/java/com/dineup/ejb/DefaultRestaurantDataSource.java
+++ DineUp-ejb/src/main/java/com/dineup/ejb/DefaultRestaurantDataSource.java
@@ -2,16 +2,35 @@ package com.dineup.ejb;
 
 import com.dineup.dom.Restaurant;
 import com.dineup.entity.RestaurantEntity;
-import java.util.Collections;
+import com.dineup.entity.RestaurantEntity_;
 import java.util.List;
 import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 @Singleton
 public class DefaultRestaurantDataSource implements RestaurantDataSource {
 
+    @PersistenceContext
+    private EntityManager manager;
+
+    private EntityManager getManager() {
+        return manager;
+    }
+    
     @Override
     public List<Restaurant> getRestaurants() {
-        return (List) Collections.singletonList(new RestaurantEntity());
+        CriteriaBuilder builder = getManager().getCriteriaBuilder();
+        CriteriaQuery<RestaurantEntity> query = builder.createQuery(RestaurantEntity.class);
+        Root<RestaurantEntity> root = query.from(RestaurantEntity.class);
+        query.orderBy(
+            builder.asc(root.get(RestaurantEntity_.key))
+        );
+        List<RestaurantEntity> restaurants = getManager().createQuery(query).getResultList();
+        return (List) restaurants;
     }
 
 }
--- DineUp-ejb/src/main/java/com/dineup/entity/RestaurantEntity.java
+++ DineUp-ejb/src/main/java/com/dineup/entity/RestaurantEntity.java
@@ -1,15 +1,29 @@
 package com.dineup.entity;
 
 import com.dineup.dom.Restaurant;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-public class RestaurantEntity implements Restaurant {
+@Entity
+@Table(name = "restaurant")
+public class RestaurantEntity implements Restaurant, Serializable {
 
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @Column(name = "key")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer key;
+    
+    @Column(name="id", unique=true, nullable=false)
     private String id;
 
     public RestaurantEntity() {
-        key = 1;
-        id = "test";
     }
 
     public Integer getKey() {
