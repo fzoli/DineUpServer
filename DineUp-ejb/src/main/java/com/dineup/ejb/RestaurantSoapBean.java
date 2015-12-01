469dc2131680fbb56fa1935868b8d44f22b6dd09
Zoltan Farkas <zoltan.farkas@cellum.com>
2015-12-01 09:02:52 +0100

    SOAP example

---
+++ DineUp-ejb/src/main/java/com/dineup/ejb/RestaurantSoapBean.java
@@ -0,0 +1,36 @@
+package com.dineup.ejb;
+
+import com.dineup.dom.Restaurant;
+import com.dineup.rest.ElementConfig;
+import com.dineup.rest.element.RestaurantElement;
+import com.dineup.rest.element.converter.RestaurantElementConverter;
+import com.dineup.util.Converters;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+/**
+ * Sample SOAP Web Service Code
+ * WSDL: http://localhost:8080/soap/Restaurant?wsdl
+ * XSD: http://localhost:8080/soap/Restaurant?xsd=1
+ */
+@Singleton
+@WebService(serviceName = "soap", name = "Restaurant", targetNamespace = "http://soap.dineup.com")
+@LocalBean
+public class RestaurantSoapBean {
+    
+    @EJB
+    private RestaurantDataSource dataSource;
+    
+    @WebMethod
+    public List<RestaurantElement> getRestaurants(String languageCode) {
+        ElementConfig elementConfig = ElementConfig.newBuilder().languageCode(languageCode).build();
+        List<Restaurant> objects = dataSource.getRestaurants();
+        List<RestaurantElement> elements = Converters.convertList(objects, new RestaurantElementConverter(elementConfig));
+        return elements;
+    }
+    
+}
