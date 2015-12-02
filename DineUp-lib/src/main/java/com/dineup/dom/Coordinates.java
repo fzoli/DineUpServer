bb126dd7ec5c1a0ed0b21b7ae877190ad5fca462
Zoltan Farkas <zoltan.farkas@cellum.com>
2015-12-02 20:38:51 +0100

    update datamodel, coordinate util

---
+++ DineUp-lib/src/main/java/com/dineup/dom/Coordinates.java
@@ -0,0 +1,21 @@
+package com.dineup.dom;
+
+public class Coordinates {
+
+    public static double getDistanceInMeter(Coordinate coordinateA, Coordinate coordinateB) {
+        double pk = (double) (180/3.14169);
+        double a1 = coordinateA.getLatitude() / pk;
+        double a2 = coordinateA.getLongitude() / pk;
+        double b1 = coordinateB.getLatitude() / pk;
+        double b2 = coordinateB.getLongitude() / pk;
+        double t1 = Math.cos(a1) * Math.cos(a2) * Math.cos(b1) * Math.cos(b2);
+        double t2 = Math.cos(a1) * Math.sin(a2) * Math.cos(b1) * Math.sin(b2);
+        double t3 = Math.sin(a1) * Math.sin(b1);
+        double tt = Math.acos(t1 + t2 + t3);
+        return 6366000 * tt;
+    }
+    
+    private Coordinates() {
+    }
+    
+}
