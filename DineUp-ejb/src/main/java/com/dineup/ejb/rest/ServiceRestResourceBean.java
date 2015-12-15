24f8046363effee86fd3dae002f5478fbc5abfb3
Zoltan Farkas <zoltan.farkas@cellum.com>
2015-12-15 20:59:56 +0100

    add version comparator; note

--- DineUp-ejb/src/main/java/com/dineup/ejb/rest/ServiceRestResourceBean.java
+++ DineUp-ejb/src/main/java/com/dineup/ejb/rest/ServiceRestResourceBean.java
@@ -2,6 +2,7 @@ package com.dineup.ejb.rest;
 
 import com.dineup.service.element.ServiceElement;
 import com.dineup.service.ServiceConfig;
+import com.dineup.util.VersionNumber;
 import java.util.List;
 import javax.ejb.Singleton;
 import javax.ws.rs.core.Response;
@@ -19,7 +20,8 @@ public class ServiceRestResourceBean implements ServiceRestResource {
     public Response getService(String apiVersion) {
         ServiceElement serviceElement = new ServiceElement();
         serviceElement.apiVersion = ServiceConfig.API_VERSION;
-        serviceElement.clientUpToDate = true; // TODO
+        int versionCompareResult = VersionNumber.versionCompare(ServiceConfig.API_VERSION, apiVersion);
+        serviceElement.clientUpToDate =  versionCompareResult <= 0;
         return Response.ok(serviceElement).build();
     }
     
