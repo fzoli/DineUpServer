6ba9a49410b86d4acf540f9b0eb89974c5fee910
Zoltan Farkas <zoltan.farkas@cellum.com>
2015-12-30 03:11:33 +0100

    make ConvertList to Serializable

--- DineUp-base/src/main/java/com/dineup/util/Lists.java
+++ DineUp-base/src/main/java/com/dineup/util/Lists.java
@@ -1,5 +1,6 @@
 package com.dineup.util;
 
+import java.io.Serializable;
 import java.util.AbstractList;
 import java.util.List;
 import java.util.ListIterator;
@@ -19,31 +20,39 @@ public class Lists {
     }
     
     public static <TCastTo, TCastFrom extends TCastTo> List<TCastTo> convert(final List<TCastFrom> list) {
-        return new AbstractList<TCastTo>() {
-            
-            @Override
-            public TCastTo get(int i) {
-                return list.get(i);
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return list.remove(o);
-            }
-
-            @Override
-            public TCastTo remove(int index) {
-                return list.remove(index);
-            }
-            
-            @Override
-            public int size() {
-                return list.size();
-            }
-            
-        };
+        return new ConvertList<>(list);
     }
 
+    private static class ConvertList<TCastTo, TCastFrom extends TCastTo> extends AbstractList<TCastTo> implements Serializable {
+        
+        private final List<TCastFrom> list;
+        
+        public ConvertList(List<TCastFrom> list) {
+            this.list = list;
+        }
+        
+        @Override
+        public TCastTo get(int i) {
+            return list.get(i);
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            return list.remove(o);
+        }
+
+        @Override
+        public TCastTo remove(int index) {
+            return list.remove(index);
+        }
+
+        @Override
+        public int size() {
+            return list.size();
+        }
+        
+    }
+    
     private Lists() {
     }
     
